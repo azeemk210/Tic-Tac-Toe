90b9317c92bd5a42d41dc55d23a7dcb852e942d7
azeemk210 <khan.azeemak@gmail.com>
2024-10-13 23:03:48 +0200

    changed

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let boxes = document.querySelectorAll('.box');
+let msg = document.querySelector('#msg');
+let msgContainer = document.querySelector('.msg-container');
 
 let turnO = true;
 
@@ -25,10 +27,8 @@ boxes.forEach((box) => {
 });
 
 const showWinner = (winner) => {
-    msg.innerText = 'Congratulations! ' + winner + ' wins!';
+    msg.innerText = 'Congratulations!, Player ' + winner + ' wins!';
     msgContainer.classList.remove("hide");
-
-
 }
 
 const checkWinner = () => {
@@ -36,14 +36,13 @@ const checkWinner = () => {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
-        
+
         if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
-            if ( pos1Val === pos2Val && pos2Val === pos3Val) {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 showWinner(pos1Val);
                 return true;
             }
         }
     }
-
 }
 
